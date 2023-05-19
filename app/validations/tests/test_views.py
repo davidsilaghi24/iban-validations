3c1ef3f4b9c32ad4ffd75b74a703955f52b1cb29
davidsilaghi24 <david.silaghi24@gmail.com>
2023-05-19 10:33:22 +0300

    Added force_authenticate to the tests

--- app/validations/tests/test_views.py
+++ app/validations/tests/test_views.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 from validations.models import IbanValidation
@@ -12,39 +11,33 @@ from validations.models import IbanValidation
 class TestIbanViews(TestCase):
 
     @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.client = APIClient()
+    def setUpTestData(cls):
+        super().setUpTestData()
         cls.validate_iban_url = reverse('validations:validate-iban')
         cls.validation_history_url = reverse('validations:validation-history')
-        cls.user = User.objects.create_user(username='testuser', password='testpassword')
-        cls.token = Token.objects.create(user=cls.user)
+        cls.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
     def test_iban_validation_view_no_iban(self):
-        response = self.client.post(
-            self.validate_iban_url,
-            data={'iban': ''},
-            HTTP_AUTHORIZATION='Token ' + self.token.key
-        )
+        response = self.client.post(self.validate_iban_url, data={'iban': ''})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {'error': 'Invalid request. Please provide the IBAN.'})
+        self.assertEqual(
+            response.data, {'error': 'Invalid request. Please provide the IBAN.'})
 
     def test_iban_validation_view_with_iban_valid(self):
-        response = self.client.post(
-            self.validate_iban_url,
-            data={'iban': 'ME25505000012345678951'},
-            HTTP_AUTHORIZATION='Token ' + self.token.key
-        )
+        response = self.client.post(self.validate_iban_url, data={
+                                    'iban': 'ME25505000012345678951'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('valid', response.data)
         self.assertTrue(response.data['valid'])
 
     def test_iban_validation_view_with_iban_invalid_format(self):
         response = self.client.post(
-            self.validate_iban_url,
-            data={'iban': 'INVALID'},
-            HTTP_AUTHORIZATION='Token ' + self.token.key
-        )
+            self.validate_iban_url, data={'iban': 'INVALID'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('valid', response.data)
         self.assertFalse(response.data['valid'])
@@ -57,10 +50,7 @@ class TestIbanViews(TestCase):
         invalid_iban = IbanValidation.objects.create(
             iban="INVALID", valid=False)
 
-        response = self.client.get(
-            self.validation_history_url,
-            HTTP_AUTHORIZATION='Token ' + self.token.key
-        )
+        response = self.client.get(self.validation_history_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Check that the response contains the 'history' key
